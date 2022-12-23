daf135726e0236533f9347d106be0b0ba0f5e522
Praveen Kumar Nihaliya <nihaliyapraveen@gmail.com>
2022-12-24 01:20:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+//declare the variable
+var hr = 0;
+var min = 0;
+var sec = 0;
+var count = 0;
+
+var timer = false;
+
+//start the functioning of the button 
+function start(){
+  timer = true;
+  stopwatch();  
+}
+
+function stop(){
+    timer=false; 
+}
+
+function reset(){
+    timer = false;
+    hr = 0;
+    min = 0;
+    sec = 0;
+    count = 0;
+
+     document.getElementById("hr").innerHTML = "00";
+     document.getElementById("sec").innerHTML = '00';
+     document.getElementById("min").innerHTML = '00';
+     document.getElementById("count").innerHTML = '00';
+}
+
+
+// here we set the increasing value of hr min sec count & then reset all to 0 
+ function stopwatch(){
+    if (timer == true){
+        count = count+1;
+
+        if(count == 100){
+            sec = sec + 1;
+            count = 0;
+        }
+
+
+        if(sec == 60){
+            min = min + 1;
+            sec = 0;
+        }
+
+
+        if(min == 60){
+            hr = hr + 1;
+            min = 0;
+            sec = 0;
+        }
+
+
+// convert the value into string
+        var hrString = hr;
+        var minString = min;
+        var secString = sec;
+        var countString = count;
+
+        if(hr<10){
+            hrString = "0" + hrString;
+        }
+
+           if(min<10){
+            minString = "0" + minString;
+        }
+
+           if(sec<10){
+            secString = "0" + secString;
+        }
+
+           if(count<10){
+            countString = "0" + countString;
+        }
+
+         
+
+// change the innerHtml into string "00"
+        document.getElementById("hr").innerHTML = hrString;
+        document.getElementById("min").innerHTML = minString;
+        document.getElementById("sec").innerHTML = secString;
+        document.getElementById("count").innerHTML = countString;
+        setTimeout("stopwatch()",10); //setTimeout calling function for stopwatch in every 10ms
+    }
+ }
